4c866a842e5192101d2da6c605a293f86775838d
othersidedrl <darelpandeyis@gmail.com>
2025-07-13 14:48:52 +0700

    feat(web): enhance TypeScript configuration and implement Portfolio component with data fetching using React Query

--- frontend/apps/web/app/layout.tsx
+++ frontend/apps/web/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-poppins',
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "Darel's Portfolio",
@@ -20,10 +21,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} antialiased`}
-      >
-        {children}
+      <body className={`${poppins.variable} antialiased`}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- frontend/apps/web/app/page.tsx
+++ frontend/apps/web/app/page.tsx
@@ -1,6 +1,166 @@
+"use client";
 
-export default function Home() {
-  return (
-    null
-  );
+import { useQuery } from "@tanstack/react-query";
+import axios from "~lib/axios";
+
+type skillLevel = ["Beginner, Intermediate", "Advanced"];
+type cateogries = ["Backend", "Frontend", "Other"];
+
+type HeroResponse = {
+  name: string;
+  rank: string;
+  title: string;
+  subtitle: string;
+  resume_link: string;
+  contact_link: string;
+  image_urls: string[];
+  hobbies: string[];
+};
+
+type Card = {
+  id: string;
+  description: string;
+};
+
+type AboutResponse = {
+  description: string;
+  cards: Card[];
+  github_link: string;
+  linkedin_link: string;
+  available: boolean;
+};
+
+type Skills = {
+  name: string;
+  description: string;
+  specialities: string[];
+  level: skillLevel;
+  category: cateogries;
+};
+
+type SkillsResponse = {
+  data: Skills[];
+  length: number;
+};
+
+type Careers = {
+  startedAt: string;
+  endedAt: string;
+  title: string;
+  affiliation: string;
+  description: string;
+  location: string;
+};
+
+type CareersRepsonse = {
+  data: Careers[];
+  length: number;
+};
+
+type Testimony = {
+  name: string;
+  profile_url: string;
+  affiliation: string;
+  rating: number;
+  description: string;
+  ai_summary: string;
+};
+
+type TestimonyItemsResponse = {
+  data: Testimony[];
+  length: number;
+};
+
+type TestimonyRepsonse = {
+  title: string;
+  description: string;
+};
+
+type Project = {
+  name: string;
+  imageUrls: string[];
+  description: string;
+  techStack: string[];
+  githubLink: string;
+  type: string;
+  contribution: string;
+  projectLink: string;
+};
+
+type ProjectItemsResponse = {
+  data: Project[];
+  length: number;
+};
+
+type ProjectResponse = {
+  title: string;
+  description: string;
+};
+
+export default function Portfolio() {
+  const { data: HeroData } = useQuery({
+    queryKey: ["hero"],
+    queryFn: async () => {
+      const response = await axios.get("/hero");
+      return response.data as HeroResponse;
+    },
+  });
+
+  const { data: AboutData } = useQuery({
+    queryKey: ["about"],
+    queryFn: async () => {
+      const response = await axios.get("/about");
+      return response.data as AboutResponse;
+    },
+  });
+
+  const { data: SkillsData } = useQuery({
+    queryKey: ["skills"],
+    queryFn: async () => {
+      const response = await axios.get("/about/skills");
+      return response.data as SkillsResponse;
+    },
+  });
+
+  const { data: CareerData } = useQuery({
+    queryKey: ["careers"],
+    queryFn: async () => {
+      const response = await axios.get("/about/careers");
+      return response.data as CareersRepsonse;
+    },
+  });
+
+  const { data: TestimonyData } = useQuery({
+    queryKey: ["testimony"],
+    queryFn: async () => {
+      const response = await axios.get("/testimony");
+      return response.data as TestimonyRepsonse;
+    },
+  });
+
+  const { data: TestimonyItems } = useQuery({
+    queryKey: ["testimony-items"],
+    queryFn: async () => {
+      const response = await axios.get("/testimony/items/approved");
+      return response.data as TestimonyItemsResponse;
+    },
+  });
+
+  const { data: ProjectData } = useQuery({
+    queryKey: ["project"],
+    queryFn: async () => {
+      const response = await axios.get("/testimony");
+      return response.data as ProjectResponse;
+    },
+  });
+
+  const { data: ProjectItems } = useQuery({
+    queryKey: ["project-items"],
+    queryFn: async () => {
+      const response = await axios.get("/project/items");
+      return response.data as ProjectItemsResponse;
+    },
+  });
+
+  return null;
 }
